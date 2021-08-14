65dd04821d9d09a250c7597ecfb9077ccbb9cb5b
Jon C <oddballbdd@hotmail.com>
2021-08-14 15:12:42 -0400

    Add angular material modules needed to be imported into each module

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatFormFieldModule,
-    MatLabel,
+    MatInputModule,
+    MatButtonModule,
     MatIconModule
   ]
 })
