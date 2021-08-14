d12eb92d4c4752a1ba5def2a3feaaf972d0c3f8f
Jon C <oddballbdd@hotmail.com>
2021-08-14 15:14:03 -0400

    Mock httpClient and handle errors via akita

--- src/app/auth/state/auth.service.ts
+++ src/app/auth/state/auth.service.ts
@@ -1,14 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AuthStore } from './auth.store';
+import { AuthState, AuthStore } from './auth.store';
+import { catchError, take, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
-  constructor(private authStore: AuthStore) {
+  constructor(private authStore: AuthStore, private httpClient: HttpClient) {
   }
 
-  updateUsername(username: string) {
-    this.authStore.update({username})
+  login(username: string) {
+    let authState: AuthState = {
+      username: username, 
+      isLoggedIn: true
+    }
+    //  Placeholder for api call
+    return of('').pipe(
+      take(1), // used to mock finite http call
+      tap(() => {
+        this.authStore.update(authState)
+      }),
+      catchError((error) => {
+        this.authStore.setError(error)
+        throw error;
+      })
+    )
   }
 
 }
