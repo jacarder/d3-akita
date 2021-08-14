8a1d53d6d1e402d0f1b0ecd65c6244142f860628
Jon C <oddballbdd@hotmail.com>
2021-08-14 14:06:55 -0400

    add missing material modules

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatLabel,
+    MatIconModule
   ]
 })
 export class AngularMaterialModule { }
