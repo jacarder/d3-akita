f05f6ab43985a90011071285725f84727cfbf1d5
Jon C <oddballbdd@hotmail.com>
2021-08-16 13:07:28 -0400

    Tool tip added & highlight friends friends

--- src/app/friend/age-weight-scatter-graph/age-weight-scatter-graph.component.ts
+++ src/app/friend/age-weight-scatter-graph/age-weight-scatter-graph.component.ts
@@ -3,6 +3,8 @@ import * as d3 from 'd3';
 import { Subscription } from 'rxjs';
 import { FriendQuery } from 'src/app/friend/state/friend.query';
 import { FriendService } from 'src/app/friend/state/friend.service';
+import { FriendValidation } from '../models/friend.validation';
+import { Friend } from '../state/friend.model';
 
 @Component({
   selector: 'age-weight-scatter-graph',
@@ -14,6 +16,8 @@ export class AgeWeightScatterGraphComponent implements OnInit, OnDestroy, AfterV
   private readonly margin = {top: 10, right: 30, bottom: 30, left: 60};
   private readonly width = 460 - this.margin.left - this.margin.right;
   private readonly height = 400 - this.margin.top - this.margin.bottom;
+  private readonly defaultColor = '#69b3a2';
+  private readonly defaultRadius = 2.5;
   private subscriptions = new Subscription();
   graphName = 'age_weight_graph'
   loading$ = this.friendQuery.selectLoading();
@@ -50,17 +54,17 @@ export class AgeWeightScatterGraphComponent implements OnInit, OnDestroy, AfterV
     this.subscriptions.add(
       this.friends$.subscribe(
         (friends) => {
-          // Add X axis
+          // Add X axis AGE
           const x = d3.scaleLinear()
-          .domain([0, 120])
+          .domain([FriendValidation.MinMaxAge.MIN, FriendValidation.MinMaxAge.MAX])
           .range([ 0, this.width ]);
           svg.append("g")
           .attr("transform", `translate(0, ${this.height})`)
           .call(d3.axisBottom(x));
   
-          // Add Y axis
+          // Add Y axis WEIGHT
           const y = d3.scaleLinear()
-          .domain([0, 600])
+          .domain([FriendValidation.MinMaxWeight.MIN, FriendValidation.MinMaxWeight.MAX])
           .range([ this.height, 0]);
           svg.append("g")
           .call(d3.axisLeft(y));
@@ -70,7 +74,7 @@ export class AgeWeightScatterGraphComponent implements OnInit, OnDestroy, AfterV
               .attr("text-anchor", "end")
               .attr("x", this.width/2 + this.margin.left)
               .attr("y", this.height + this.margin.top + 20)
-              .text("Friends Age");
+              .text("Age (years)");
   
           // Y axis label:
           svg.append("text")
@@ -78,17 +82,85 @@ export class AgeWeightScatterGraphComponent implements OnInit, OnDestroy, AfterV
               .attr("transform", "rotate(-90)")
               .attr("y", -this.margin.left + 20)
               .attr("x", -this.margin.top - this.height/2 + 20)
-              .text("Friends Weight")
+              .text("Weight (pounds)")
+
+          //  Highlight related friends
+          const highlight = (event: any,f: Friend) => {
+            f.friendList.forEach(friend => {
+              d3.selectAll("." + friend.id)
+                .transition()
+                .duration(200)
+                .style("fill", 'red')
+                .attr("r", 3)
+            });
+          }
+          
+          const doNotHighlight = (event: any,f: Friend) => {
+            d3.selectAll(".dot")
+              .transition()
+              .duration(200)
+              .style("fill", this.defaultColor )
+              .attr("r", this.defaultRadius )
+          }
+
+          //  Add tooltip to display information
+          const tooltip = d3.select(`#${this.graphName}`)
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "1px")
+            .style("border-radius", "5px")
+            .style("padding", "10px")
+            .style("position", "absolute")
   
           // Add dots
           svg.append('g')
           .selectAll("dot")
           .data(friends)
           .join("circle")
+              .attr("class", function (f) { return "dot " + f.id } )
               .attr("cx", function (f) { return x(f.age); } )
               .attr("cy", function (f) { return y(f.weight); } )
-              .attr("r", 1.5)
-              .style("fill", "#69b3a2");
+              .attr("r", this.defaultRadius)
+              .style("fill", this.defaultColor )
+          .on("mouseover", (event, f: Friend) => {
+            highlight(event, f);
+            //  Show tooltip
+            tooltip.style("opacity", 1);
+          })
+          .on("mousemove", (event, f: Friend) => {
+            //  Move tool tip
+            tooltip
+              .html(
+                `
+                Name: ${f.name}<br>
+                Age: ${f.age}<br>
+                Weight: ${f.weight}<br>
+                Friend Count: ${f.friendList.length}
+                `
+              )
+              .style("left", (event.x)/2 + "px")
+              .style("top", (event.y)/2 + "px")
+          })
+          .on("mouseleave", (event, f: Friend) => {
+            doNotHighlight(event, f);
+            //  Remove tooltip
+            tooltip
+              .transition()
+              .duration(200)
+              .style("opacity", 0)
+          })
+          // .on("mouseover", function(f: Friend) {
+          //   var dotId = f.id
+
+          //   var matches = svg.filter((f: Friend) => { 
+          //      return f.friendList.some(nextFriend => nextFriend.id === dotId);
+          //   })
+          //   .style('stroke', 'black');
+          // })
+     
         }
       )    
     )
